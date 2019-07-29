df24af23bee10599fb409e09d1e518c6c7e7017d
phs1234 <timemarket777@naver.com>
2019-07-29 14:22:03 +0900

    스토리 완성

--- js/story.js
+++ js/story.js
@@ -1,19 +1,9 @@
+var isNextClicked = 0;
+
 $(document).ready(function () {
-    $("#prev_arrow").click(function () {
-        pauseVideo();
-        prevPage();
-        playVideo();
-    });
-
-    $("#next_arrow").click(function () {
-        pauseVideo();
-        nextPage();
-        playVideo();
-    });
-
-    $("#back_key").click(function () {
-        changeURL('./index.html');
-    });
+    var season1 = document.getElementById('season1_video');
+    var season2 = document.getElementById('season2_video');
+    var season3 = document.getElementById('season3_video');
 
     $('#main_container').on('scroll touchmove mousewheel', function (event) {
         event.preventDefault();
@@ -21,75 +11,74 @@ $(document).ready(function () {
         return false;
     });
 
-    $('#season1').on('ended', function() {
-        nextPage();
-        playVideo();
-    });
-    
-    $('#season2').on('ended', function() {
+    $('.next').click(function () {
+        pauseVideo();
         nextPage();
         playVideo();
     });
 
-    $('#season3').on('ended', function() {
+    $('.home').click(function () {
+        pauseVideo();
+        toHome();
+    });
+
+    $('#season1_video, #season2_video').on('ended', function () {
         nextPage();
         playVideo();
     });
 
+    $('#season3_video').on('ended', function () {
+        toHome();
+    });
 
+    $("#season1_video").on("timeupdate", function(event){
+        $('#season1_progress').css('width', `${(season1.currentTime / season1.duration) * 100}%`);
+    });
+
+    $("#season2_video").on("timeupdate", function(event){
+        $('#season2_progress').css('width', `${(season2.currentTime / season2.duration) * 100}%`);
+    });
+
+    $("#season3_video").on("timeupdate", function(event){
+        $('#season3_progress').css('width', `${(season3.currentTime / season3.duration) * 100}%`);
+    });
 });
 
-function pauseVideo() {
-    $(`${location.hash}`).get(0).pause();
-}
+$(window).scroll(function() {
+    var scroll = $(document).scrollLeft();
+    console.log($(document).scrollLeft());
+
+    if (scroll <= window.innerWidth) {
+        $("#season1_topic").addClass("fade_out");
+    } else if (scroll <= window.innerWidth * 2) {
+       
+    } else if (scroll <= window.innerWidth * 3) {
+       
+    }
+});
+
+window.onload = function() {playVideo()};
 
 function playVideo() {
-    $(`${location.hash}`).get(0).play();
+    console.log(`#season${isNextClicked + 1}_video`);
+    $(`#season${isNextClicked + 1}_video`).get(0).play();
 }
 
-function getSeason() {
-    return Number(location.hash.charAt(location.hash.length - 1));
-}
-
-function scrollToHash(hash) {
-    $('html,body').animate({scrollTop:$(hash).offset().top}, 300);
-}
-
-function changeURL(url) {
-    window.location.href = url;
-}
-
-function changeNumber() {
-    $("#season_number").html(getSeason());
-}
-
-function prevPage() {
-    var season = getSeason();
-
-    if (season === 1){
-        scrollToHash('#season3');
-        changeURL('#season3');
-    }else{
-        scrollToHash('#season' + (season - 1));
-        changeURL('#season' + (season - 1));
-    }
-
-    changeNumber();
+function pauseVideo() {
+    console.log(`#season${isNextClicked + 1}_video`);
+    $(`#season${isNextClicked + 1}_video`).get(0).pause();
 }
 
 function nextPage() {
-    var season = getSeason();
-
-    if (season === 3){
-        scrollToHash('#season1');
-        changeURL('#season1');
-    } else {
-        scrollToHash('#season' + (season + 1));
-        changeURL('#season' + (season + 1));
-    }
-
-
-    changeNumber();
+    console.log(`#season${isNextClicked + 1}_video`);
+    $('#main_container').css('margin-left', `-${++isNextClicked * 100}vw`);
 }
+
+function toHome() {
+    window.location.href = "./index.html";
+}
+
+
+
 
 
