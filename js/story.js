dcbf3aaf119e97515a421575744f54dfe5346a20
phs1234 <timemarket777@naver.com>
2019-07-29 17:46:11 +0900

    스토리 페이드인 추가

--- js/story.js
+++ js/story.js
@@ -13,51 +13,59 @@ $(document).ready(function () {
 
     $('.next').click(function () {
         pauseVideo();
+        fadeOut();
         nextPage();
         playVideo();
+        fadeIn();
     });
 
     $('.home').click(function () {
+        fadeOut();
         pauseVideo();
         toHome();
     });
 
     $('#season1_video, #season2_video').on('ended', function () {
+        fadeOut();
         nextPage();
         playVideo();
+        fadeIn();
     });
 
     $('#season3_video').on('ended', function () {
+        fadeOut();
         toHome();
     });
 
-    $("#season1_video").on("timeupdate", function(event){
+    $("#season1_video, #season2_video, #season3_video").on("timeupdate", function(event){
         $('#season1_progress').css('width', `${(season1.currentTime / season1.duration) * 100}%`);
-    });
-
-    $("#season2_video").on("timeupdate", function(event){
         $('#season2_progress').css('width', `${(season2.currentTime / season2.duration) * 100}%`);
-    });
-
-    $("#season3_video").on("timeupdate", function(event){
         $('#season3_progress').css('width', `${(season3.currentTime / season3.duration) * 100}%`);
     });
 });
 
-$(window).scroll(function() {
-    var scroll = $(document).scrollLeft();
-    console.log($(document).scrollLeft());
+window.onload = function() {
+    playVideo();
+    fadeIn();
+};
 
-    if (scroll <= window.innerWidth) {
-        $("#season1_topic").addClass("fade_out");
-    } else if (scroll <= window.innerWidth * 2) {
-       
-    } else if (scroll <= window.innerWidth * 3) {
-       
-    }
-});
+// 보여지기
+function fadeIn() {
+    console.log(`#season${isNextClicked + 1} 보여지기`);
+    $(`#season${isNextClicked + 1}_topic`).hide().fadeIn(500);
+    $(`#season${isNextClicked + 1}_description`).hide().fadeIn(1000);
+    $(`#season${isNextClicked + 1}_next`).hide().fadeIn(1500);
+    $(`#season${isNextClicked + 1}_progress`).hide().fadeIn(2000);
+}
 
-window.onload = function() {playVideo()};
+// 사라지기
+function fadeOut() {
+    console.log(`#season${isNextClicked + 1} 사라지기`);
+    $(`#season${isNextClicked + 1}_topic`).fadeOut(500);
+    $(`#season${isNextClicked + 1}_description`).fadeOut(1000);
+    $(`#season${isNextClicked + 1}_next`).fadeOut(1500);
+    $(`#season${isNextClicked + 1}_progress`).fadeOut(2000);
+}
 
 function playVideo() {
     console.log(`#season${isNextClicked + 1}_video`);
@@ -77,6 +85,8 @@ function nextPage() {
 function toHome() {
     window.location.href = "./index.html";
 }
+
+
 
 
 
